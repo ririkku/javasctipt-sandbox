8d6698095560eb56c59b28af0c2e3a318afa5dc1
kikukawa_ryoji <ryoji19950109@gmail.com>
2018-05-29 16:41:03 +0900

    分割代入なるもん

--- src/lib/string/string.js
+++ src/lib/string/string.js
@@ -19,9 +19,20 @@ function tag(a, b, c, d){
     console.log(a);
 
     // 二つ目以降に埋め込まれた変数が代入されている。
+    // 定義されていない場合は、undefinedになるよ。
     console.log(b);
     console.log(c);
     console.log(d);
 }
 
 tag`わたしの名前は${name}です。${age}歳です。`;
+
+
+// 分割代入なるもの。
+let [n1, n2] = [100, 200];
+console.log(n1);
+console.log(n2);
+[n1, n2] = [n2, n1]; // 入れ替えも一行でできるようになってしまって。。。
+console.log(n1);
+console.log(n2);
+
