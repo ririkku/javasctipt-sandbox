b6b21fdc255fbf36f3ef8e54fb0b0b0c90fb4245
kikukawa_ryoji <ryoji19950109@gmail.com>
2018-05-29 16:26:38 +0900

    タグ付きテンプレートなるものを、、、。

--- src/lib/string/string.js
+++ src/lib/string/string.js
@@ -1,8 +1,8 @@
+// 文字列埋込
 let name = 'Kikukawa';
 let age = '23';
 let str = `わたしの名前は${name}です。${age}歳です。`;
-
-alert(str);
+console.log(str);
 
 // テンプレート文字列
 // バッククォートでくくる
@@ -10,4 +10,18 @@ alert(str);
 let str2 = `あ
 い
 う`;
-alert(str2);
+console.log(str2);
+
+
+// タグ付きテンプレートとかいうやつ
+function tag(a, b, c, d){
+    // 一つ目の引数に、通常文字列が配列で代入されている。
+    console.log(a);
+
+    // 二つ目以降に埋め込まれた変数が代入されている。
+    console.log(b);
+    console.log(c);
+    console.log(d);
+}
+
+tag`わたしの名前は${name}です。${age}歳です。`;
